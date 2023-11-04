0121e66738f6a9d2eee9bdeb456fe610967faace
Daniel Denkha <danialdnkha@gmail.com>
2023-11-04 19:00:33 +1100

    creating different fonts styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useState } from 'react';
+
+interface Dictionary {
+  font: string;
+}
+
 function App() {
-  return <div>hello</div>;
+  const [dictionary, setDictionary] = useState<Dictionary>({
+    font: 'mono',
+  } as Dictionary);
+
+  return <div className={dictionary.font}>heloo</div>;
 }
 
 export default App;
